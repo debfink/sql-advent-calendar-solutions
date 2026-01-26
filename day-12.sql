74826f92caea6d94a4086a3df69a30464b39c55d
Deborah Simmonds <93883140+debfink@users.noreply.github.com>
2026-01-26 22:26:41 +0000

    Update day-12.sql

--- day-12.sql
+++ day-12.sql
@@ -21,25 +21,28 @@
 
 -- My Solution:
 
+--create a CTE table to find the information that we need to perform the ranking
 WITH daily_counts AS (
   SELECT
-    date(nm.sent_at) AS sent_date
+    date(nm.sent_at) AS sent_date  -- convert from date/time to day
     ,nu.user_id
     ,nu.user_name
-    ,COUNT(nm.message_id) AS message_count
+    ,COUNT(nm.message_id) AS message_count -- count messages per sender 
   FROM npn_users nu
   JOIN npn_messages nm 
-    on nu.user_id = nm.sender_id
-  GROUP BY 
-    sent_date,
-    nu.user_id,
+    on nu.user_id = nm.sender_id  -- Join messages to users
+    GROUP BY 
+    sent_date, -- group these by date
+    nu.user_id, -- group these by user
     nu.user_name
 ),
+-- pick the top senders by day
+-- using rank instead of row number to show ALL top senders if tied
 ranked_senders AS (
   SELECT
   *,
-  RANK() OVER (
-    PARTITION BY sent_date
+  RANK() OVER (  
+    PARTITION BY sent_date -- restart the ranking every day
     ORDER BY message_count DESC
   ) AS rn 
   FROM daily_counts
@@ -50,4 +53,4 @@ SELECT
   user_name,
   message_count
 FROM ranked_senders
-WHERE rn = 1;
+WHERE rn = 1;  -- only return top ranked senders per day
